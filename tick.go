1a2abfbbf7663c32b88fb07f27c835bdcec1beff
John Olmsted <john@olmsted.io>
2021-05-22 11:12:07 -0400

    Add quiet and extra quiet modes
    Quiet mode suppresses the countdown output. Extra quiet mode suppresses
    both the countdown, and the final bell.

--- tick.go
+++ tick.go
@@ -11,23 +11,33 @@ import (
 //
 //     tick -m 1 -s 30
 func main() {
-	seconds := flag.Int("s", 0, "Number of seconds the timer should run")
-	minutes := flag.Int("m", 0, "Number of minutes the timer should run")
+	seconds := flag.Int("s", 0, "Number of seconds the timer should run.")
+	minutes := flag.Int("m", 0, "Number of minutes the timer should run.")
+	q := flag.Bool("q", false, "Quiet mode, suppresses countdown.")
+	qq := flag.Bool("qq", false, "Extra quiet mode, suppresses countdown and bell.")
 	flag.Parse()
 
 	duration := (time.Duration(*seconds) * time.Second) + (time.Duration(*minutes) * time.Minute)
 	ding := time.After(duration)
 	tick := time.Tick(1 * time.Second)
+	noCountdown := *q || *qq
+	noBell := *qq
 
 	for {
 		select {
 		case <-tick:
-			fmt.Printf("\033[2K\r%s", duration)
+			if !noCountdown {
+				fmt.Printf("\033[2K\r%s", duration)
+			}
 			duration = duration - (1 * time.Second)
 		case <-ding:
-			fmt.Printf("\033[2K\r%s", duration)
-			fmt.Print("\a")
-			fmt.Println("\nDING!")
+			if !noCountdown {
+				fmt.Printf("\033[2K\r%s", duration)
+				fmt.Println("\nDING!")
+			}
+			if !noBell {
+				fmt.Print("\a")
+			}
 			return
 		}
 	}
